1d47baea9b98956f286d4fabba505158737bac7f
Nícolas 'eowfenth' Deçordi dos Reis <eowfenth.services@gmail.com>
2018-04-22 13:02:07 -0300

    chore: remove __test__ path

--- __test__/index.js
+++
@@ -1,9 +0,0 @@
-import 'react-native';
-import renderer from 'react-test-renderer';
-import React from 'react';
-import App from '../App';
-
-it('renders correctly', () => {
-  const tree = renderer.create(<App />);
-  expect(tree).toMatchSnapshot();
-});
